6ec374ac89e62d099ef2ac127f8637aae1c39054
frank <frankzye@qq.com>
2022-11-21 22:09:17 +0800

    remove error

--- resource_server.go
+++ resource_server.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	compute "cloud.google.com/go/compute/apiv1"
 	"context"
 	"fmt"
+
+	compute "cloud.google.com/go/compute/apiv1"
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	computepb "google.golang.org/genproto/googleapis/cloud/compute/v1"
@@ -45,10 +46,7 @@ func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
 	uid, _ := uuid.NewUUID()
 	// delete resource policy no matter exist or not
 	deleteResourcePolicy(project, zone, instance, policy)
-	err := addResourcePolicy(project, zone, instance, policy)
-	if err != nil {
-		return err
-	}
+	addResourcePolicy(project, zone, instance, policy)
 	d.SetId(uid.String())
 	return nil
 }
@@ -64,15 +62,8 @@ func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
 		instance, newInstance := d.GetChange("instance")
 		policy, newPolicy := d.GetChange("policy")
 
-		err := deleteResourcePolicy(project, zone.(string), instance.(string), policy.(string))
-		if err != nil {
-			return err
-		}
-
-		err = addResourcePolicy(project, newZone.(string), newInstance.(string), newPolicy.(string))
-		if err != nil {
-			return err
-		}
+		deleteResourcePolicy(project, zone.(string), instance.(string), policy.(string))
+		addResourcePolicy(project, newZone.(string), newInstance.(string), newPolicy.(string))
 	}
 	return nil
 }
@@ -82,20 +73,18 @@ func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
 	zone := d.Get("zone").(string)
 	instance := d.Get("instance").(string)
 	policy := d.Get("policy").(string)
-	err := deleteResourcePolicy(project, zone, instance, policy)
-	if err != nil {
-		return err
-	}
+	deleteResourcePolicy(project, zone, instance, policy)
 	return nil
 }
 
-func addResourcePolicy(projectID string, zone string, instance string, policy string) error {
+func addResourcePolicy(projectID string, zone string, instance string, policy string) {
 	ctx := context.Background()
 	uid, _ := uuid.NewUUID()
 	requestId := uid.String()
 	instanceClient, err := compute.NewInstancesRESTClient(ctx)
 	if err != nil {
-		return fmt.Errorf("NewInstancesRESTClient: %v", err)
+		fmt.Println("NewInstancesRESTClient:", err)
+		return
 	}
 	req := &computepb.AddResourcePoliciesInstanceRequest{
 		Project:   projectID,
@@ -106,22 +95,24 @@ func addResourcePolicy(projectID string, zone string, instance string, policy st
 	}
 	op, err := instanceClient.AddResourcePolicies(ctx, req)
 	if err != nil {
-		return fmt.Errorf(err.Error())
+		fmt.Println(err.Error())
+		return
 	}
 	waitErr := op.Wait(ctx)
 	if waitErr != nil {
-		return fmt.Errorf(waitErr.Error())
+		fmt.Println(waitErr.Error())
 	}
-	return nil
+	return
 }
 
-func deleteResourcePolicy(projectID string, zone string, instance string, policy string) error {
+func deleteResourcePolicy(projectID string, zone string, instance string, policy string) {
 	ctx := context.Background()
 	uid, _ := uuid.NewUUID()
 	requestId := uid.String()
 	instanceClient, err := compute.NewInstancesRESTClient(ctx)
 	if err != nil {
-		return fmt.Errorf("NewInstancesRESTClient: %v", err)
+		fmt.Println("NewInstancesRESTClient:", err)
+		return
 	}
 	req := &computepb.RemoveResourcePoliciesInstanceRequest{
 		Project:   projectID,
@@ -132,11 +123,11 @@ func deleteResourcePolicy(projectID string, zone string, instance string, policy
 	}
 	op, err := instanceClient.RemoveResourcePolicies(ctx, req)
 	if err != nil {
-		return fmt.Errorf(err.Error())
+		fmt.Println(err.Error())
+		return
 	}
 	waitErr := op.Wait(ctx)
 	if waitErr != nil {
-		return fmt.Errorf(waitErr.Error())
+		fmt.Println(waitErr.Error())
 	}
-	return nil
 }
